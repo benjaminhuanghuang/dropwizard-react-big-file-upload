0ce0291cd2171f63d05fbb15bb5a8823b0823bc3
Benjamin Huang <bhuang@rms.com>
2019-03-28 16:58:26 -0700

    GET works

--- dropwizard-rest-api/src/main/java/cn/ben/dropwizardrestapi/resources/FileResource.java
+++ dropwizard-rest-api/src/main/java/cn/ben/dropwizardrestapi/resources/FileResource.java
@@ -1,44 +1,53 @@
 package cn.ben.dropwizardrestapi.resources;
 
-import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 import java.io.*;
-import java.net.URI;
-import java.util.List;
 
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
+//import com.sun.jersey.core.header.FormDataContentDisposition;
+//import com.sun.jersey.multipart.FormDataParam;
+
 
 @Path("/file")
 @Produces(MediaType.APPLICATION_JSON)
 public class FileResource {
-    @POST
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response uploadPdfFile(@FormDataParam("file") InputStream fileInputStream,
-                                  @FormDataParam("file") FormDataContentDisposition fileMetaData) throws Exception {
-        String UPLOAD_PATH = "./ben-temp/";
+//    @POST
+//    @Consumes(MediaType.MULTIPART_FORM_DATA)
+//    public Response upload(@FormDataParam("file") InputStream fileInputStream,
+//                                  @FormDataParam("file") FormDataContentDisposition fileMetaData) throws Exception {
+//        String UPLOAD_PATH = "./ben-temp/";
+//        String uploadedFileLocation = UPLOAD_PATH + fileMetaData.getFileName();
+//
+//        // save it
+//        writeToFile(fileInputStream, uploadedFileLocation);
+//
+//        return Response.ok("Data uploaded successfully !!").build();
+//    }
+    // save uploaded file to new location
+    private void writeToFile(InputStream uploadedInputStream,
+                             String uploadedFileLocation) {
+
         try {
+            OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
             int read = 0;
             byte[] bytes = new byte[1024];
 
-            OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + fileMetaData.getFileName()));
-            while ((read = fileInputStream.read(bytes)) != -1) {
+            out = new FileOutputStream(new File(uploadedFileLocation));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
                 out.write(bytes, 0, read);
             }
             out.flush();
             out.close();
         } catch (IOException e) {
-            throw new WebApplicationException();
+
+            e.printStackTrace();
         }
-        return Response.ok("Data uploaded successfully !!").build();
+
     }
 
     @GET
     public Response hello() {
-        return Response.ok("Success!").build();
+        return Response.ok("Hello Dropwizard!").build();
     }
 }
---
+++ dropwizard-rest-api/src/test/java/cn/ben/dropwizardrestapi/resources/FileUploadIT.java
@@ -0,0 +1,37 @@
+package cn.ben.dropwizardrestapi.resources;
+
+import org.junit.Test;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+
+public class FileUploadIT {
+
+    @Test
+    public void uploadPdfFile() {
+        //
+        String inputFilePath = "";
+        byte[] inputBytes = null;
+        try {
+            inputBytes = Files.readAllBytes(new File(inputFilePath).toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //
+        Client client = ClientBuilder.newClient();
+
+        WebTarget target = client.target("http://localhost:4444/file");
+        //target = target.queryParam("pass", "mypass123");
+        target = target.queryParam("inputFileName", "bigfile.bin");
+        Invocation.Builder builder = target.request(MediaType.MULTIPART_FORM_DATA);
+
+        Response resp = builder.put(Entity.entity(inputBytes, MediaType.MULTIPART_FORM_DATA));
+        System.out.println("response = " + resp.getStatus());
+
+    }
+}
